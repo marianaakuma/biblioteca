ddf25d75cf0f80047006be2e0e5fa82574e7ecd2
albalopes <alba.lopes@ifrn.edu.br>
2024-07-23 14:26:22 +0000

    Exemplo de uso de dicionário e laço de repetição

--- app.py
+++ app.py
@@ -8,4 +8,18 @@ def index():
 
 @app.route('/cardapio')
 def cardapio():
-    return render_template('cardapio.html')
+
+    pizzas = [{'sabor': 'Camarão', 'preco': '150', 'imagem': 'camarao.webp'},
+              {'sabor': 'Calabreza', 'preco': '50', 'imagem': 'calabreza.jfif'},
+              {'sabor': 'Nordestina', 'preco': '60', 'imagem': 'nordestina.jpg'},
+              {'sabor': 'Frango com Catupiry', 'preco': '55', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Quatro queijos', 'preco': '70', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Marguerita', 'preco': '60', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Havaiana', 'preco': '65', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Três quejos', 'preco': '45', 'imagem': 'pizza.jpg'}, 
+              {'sabor': 'Portugesa', 'preco': '60', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Doce de leite', 'preco': '50', 'imagem': 'pizza.jpg'},
+              {'sabor': 'Chocolate', 'preco': '50', 'imagem': 'pizza.jpg'},
+            ]
+
+    return render_template('cardapio.html', pizzas=pizzas)
