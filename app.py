f6d502ef15001e831214dcd118cc58b6f1436988
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2024-08-06 13:37:55 +0000

    salva

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ def index():
 @app.route('/generos')
 def generos():
 
-    Livros = [{'titulos': 'Dom Casmurro','1899','imagem': 'camarao.webp'},
-              {'titulos': ' George Orwell ','1984','imagem':'calabreza.jfif'},
-              {'titulos': 'O Sol é para Todos','1960','imagem':'nordestina.jpg'},
-              {'titulos': 'Cem Anos de Solidão','(1967','imagem': 'pizza.jpg'},
-              {'titulos': 'O Senhor dos Anéis: A Sociedade do Anel','1954','imagem': 'pizza.jpg'},
-              {'titulos': 'The Great Gatsby','1925','imagem': 'pizza.jpg'},
-              {'titulos': 'Orgulho e Preconceito','1813','imagem': 'pizza.jpg'},
-              {'titulos': 'O Apanhador no Campo de Centeio','1951','imagem': 'pizza.jpg'}, 
-              {'titulos': 'A Revolução dos Bichos','1945','imagem': 'pizza.jpg'},
-              {'titulos': 'O Pequeno Príncipe','1943','imagem': 'pizza.jpg'},
-              {'titulos': 'A Menina que Roubava Livros','2005','imagem': 'pizza.jpg'},
+    Livros = [{'titulos': 'Dom Casmurro','1899','imagem': 'Dom Casmurro.jpg'},
+              {'titulos': ' George Orwell ','1984','imagem':'George Orwell.jpg'},
+              {'titulos': 'O Sol é para Todos','1960','imagem':'O Sol é para Todos.jpg'},
+              {'titulos': 'Cem Anos de Solidão','1967','imagem': 'Cem Anos de Solidão.jpg'},
+              {'titulos': 'O Senhor dos Anéis: A Sociedade do Anel','1954','imagem': 'O Senhor dos Anéis.jpg'},
+              {'titulos': 'The Great Gatsby','1925','imagem': 'The Great Gatsby.jpg'},
+              {'titulos': 'Orgulho e Preconceito','1813','imagem': 'Orgulho e Preconceito.jpg'},
+              {'titulos': 'O Apanhador no Campo de Centeio','1951','imagem': 'O Apanhador no Campo de Centeio.jpg'}, 
+              {'titulos': 'A Revolução dos Bichos','1945','imagem': 'A Revolução dos Bichos.jpg'},
+              {'titulos': 'O Pequeno Príncipe','1943','imagem': 'O Pequeno Príncipe.jpg'},
+              {'titulos': 'A Menina que Roubava Livros','2005','imagem': 'A Menina que Roubava Livros.jpg'},
             ]
 
     return render_template('generos.html', livros=livros)
