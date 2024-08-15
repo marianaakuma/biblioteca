0888b285d687b999bcabc6a46311d57965fba25a
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2024-08-15 20:15:17 +0000

    salva

--- app.py
+++ app.py
@@ -4,36 +4,65 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sua-palavra-secreta'
 
 
-#Página inicial do site
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#Página que exibe a lista de livros disponíveis
-@app.route('/generos')
-def generos():
-
-    livros = [
-    {'id': 1, 'titulo': 'Dom Casmurro', 'ano': '1899', 'imagem': 'Dom Casmurro.jpg'},
-    {'id': 2, 'titulo': '1984', 'ano': '1949', 'imagem': 'George Orwell.jpg'},
-    {'id': 3, 'titulo': 'O Sol é para Todos', 'ano': '1960', 'imagem': 'O Sol é para Todos.jpg'},
-    {'id': 4, 'titulo': 'Cem Anos de Solidão', 'ano': '1967', 'imagem': 'Cem Anos de Solidão.jpg'},
-    {'id': 5, 'titulo': 'O Senhor dos Anéis: A Sociedade do Anel', 'ano': '1954', 'imagem': 'O Senhor dos Anéis.jpg'},
-    {'id': 6, 'titulo': 'The Great Gatsby', 'ano': '1925', 'imagem': 'The Great Gatsby.jpg'},
-    {'id': 7, 'titulo': 'Orgulho e Preconceito', 'ano': '1813', 'imagem': 'Orgulho e Preconceito.jpg'},
-    {'id': 8, 'titulo': 'O Apanhador no Campo de Centeio', 'ano': '1951', 'imagem': 'O Apanhador no Campo de Centeio.jpg'},
-    {'id': 9, 'titulo': 'A Revolução dos Bichos', 'ano': '1945', 'imagem': 'A Revolução dos Bichos.jpg'},
-    {'id': 10, 'titulo': 'O Pequeno Príncipe', 'ano': '1943', 'imagem': 'O Pequeno Príncipe.jpg'},
-    {'id': 11, 'titulo': 'A Menina que Roubava Livros', 'ano': '2005', 'imagem': 'A Menina que Roubava Livros.jpg'},
-]
-
-    return render_template('generos.html', livros=livros)
+@app.route('/integrantes')
+def integrantes():
+    return render_template('integrates.html')
 
 
-@app.route('/logi')
-def logi():
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        if email == "usuario@example.com" and senha == "senha123":
+            return redirect(url_for('usuario')) 
+        else:
+            return "Credenciais inválidas", 
+        
     return render_template('logi.html')
 
+
+@app.route('/usuario')
+def usuario():
+    return render_template('biblioteca.html')
+
+
+@app.route('/pagina-inicial')
+def pagina_inicial():
+    return render_template('pagina_inicial.html')
+
+
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+    confirmar_senha = request.form['confirmar-senha']
+    
+    if senha != confirmar_senha:
+        flash('As senhas não coincidem. Tente novamente.')
+        return redirect(url_for('cadastro'))
+
+    flash('Cadastro realizado com sucesso!')
+    return redirect(url_for('cadastro'))
+
+
+@app.route('/adm')
+def adm():
+    return render_template('adm.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
