49bad4aa59ea0d67c3269fa3e4dd3521d3b8acfe
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2024-08-20 17:22:13 +0000

    salva

--- app.py
+++ app.py
@@ -3,37 +3,34 @@ from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sua-palavra-secreta'
 
-@app.route('/')
-def index():
-    return render_template('pagina inicial.html')
+@app.route('/pagina inicial')
+def paginainicial():
+    return render_template('/pagina inicial.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('/login.html')
 
 
 @app.route('/cadastro')
 def cadastro():
-    return render_template('cadastro.html')
+    return render_template('/cadastro.html')
 
 
-@app.route('/')
+@app.route('/usuario')
 def usuario():
-    return render_template('usuario.html')
+    return render_template('/usuario.html')
 
-@app.route('/')
-def paginainicial():
-    return render_template('paginainicial.html')
 
-@app.route('/')
+@app.route('/adm')
 def adm():
-    return render_template('adm.html')
+    return render_template('/adm.html')
 
 
-@app.route('/')
-def integrate():
-    return render_template('integrate.html')
+@app.route('/sobrenos')
+def sobrenos():
+    return render_template('/sobrenos.html')
 
 
 if __name__ == '__main__':
