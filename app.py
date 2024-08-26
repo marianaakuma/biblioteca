580b1ae9a2bbdf746cad64458c4aa1c1265b6eec
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2024-08-26 22:21:38 +0000

    salva

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sua-palavra-secreta'
 
+
 @app.route('/pagina inicial')
 def paginainicial():
     return render_template('/pagina inicial.html')
-
 
 @app.route('/login')
 def login():
@@ -17,7 +17,6 @@ def login():
 def cadastro():
     return render_template('/cadastro.html')
 
-
 @app.route('/usuario')
 def usuario():
     return render_template('/usuario.html')
@@ -26,11 +25,6 @@ def usuario():
 @app.route('/adm')
 def adm():
     return render_template('/adm.html')
-
-
-@app.route('/sobrenos')
-def sobrenos():
-    return render_template('/sobrenos.html')
 
 
 if __name__ == '__main__':
